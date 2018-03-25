b04d27d0951e15f31fb67b732d1a543e4868342f
SidFrost <siddhantsahare25@gmail.com>
2018-03-25 12:28:57 +0530

    initial UI's

---
+++ ChatPro/ChatPro/Controller/ChatVC.swift
@@ -0,0 +1,35 @@
+//
+//  ChatVC.swift
+//  ChatPro
+//
+//  Created by Siddhant  Sahare on 25/03/18.
+//  Copyright © 2018 Siddhant Sahare. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ChatPro/ChatPro/View/GradientView.swift
@@ -0,0 +1,44 @@
+//
+//  GradientView.swift
+//  ChatPro
+//
+//  Created by Siddhant  Sahare on 25/03/18.
+//  Copyright © 2018 Siddhant Sahare. All rights reserved.
+//
+//
+//  Gradient.swift
+//  ChatPro
+//
+//  Created by Siddhant  Sahare on 25/03/18.
+//  Copyright © 2018 Siddhant Sahare. All rights reserved.
+//
+
+
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+    
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var bottomColor: UIColor = #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
+
+
